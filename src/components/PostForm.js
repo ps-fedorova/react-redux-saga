3d933841f5fc4fdf24310e78c829288013cbd76f
Polina Fedorova <ps.fedorova@yandex.ru>
2021-02-11 12:52:37 +0500

    first commit

---
+++ src/components/PostForm.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {createPost, showAlert} from '../redux/actions'
+import {Alert} from './Alert'
+
+class PostForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: ''
+    }
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+
+    const {title} = this.state
+
+    if (!title.trim()) {
+      return this.props.showAlert('Название поста не может быть пустым')
+    }
+
+    const newPost = {
+      title, id: Date.now().toString()
+    }
+
+    this.props.createPost(newPost)
+    this.setState({ title: '' })
+
+  }
+
+  changeInputHandler = event => {
+    event.persist()
+    this.setState(prev => ({...prev, ...{
+      [event.target.name]: event.target.value
+    }}))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.submitHandler}>
+        <div className="form-group">
+          <label htmlFor="title">Напишите пост</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={this.state.title}
+            name="title"
+            onChange={this.changeInputHandler}
+          />
+        </div>
+        {this.props.alert && <Alert text={this.props.alert} />}
+        <button className="btn btn-success" type="submit">Создать</button>
+      </form>
+    )
+  }
+}
+
+const mapDispatchToProps = {
+  createPost, showAlert
+}
+
+const mapStateToProps = state => ({
+  alert: state.app.alert
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostForm)
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {compose, createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {rootReducer} from './redux/rootReducer'
+import {forbiddenWordsMiddleware} from './redux/middleware'
+import {sagaWatcher} from './redux/sagas'
+
+const saga = createSagaMiddleware()
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk, forbiddenWordsMiddleware, saga
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+    // : () => console.log('расширение REDUX_DEVTOOLS_EXTENSION не найдено')
+))
+
+saga.run(sagaWatcher)
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+render(app, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
