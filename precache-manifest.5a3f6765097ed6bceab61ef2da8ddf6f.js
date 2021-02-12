75662cab55021a33c3b6486b76739606a4728432
Polina Fedorova <ps.fedorova@yandex.ru>
2021-02-12 13:09:26 +0500

    Updates

---
+++ precache-manifest.5a3f6765097ed6bceab61ef2da8ddf6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64a43b3113a344effcf6cd8523c548b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "900213b6747507c302ab",
+    "url": "./static/js/2.d42086f9.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "./static/js/2.d42086f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a826b8a4e9a97ee0f5",
+    "url": "./static/js/main.18526998.chunk.js"
+  },
+  {
+    "revision": "649fb693e26755593ae7",
+    "url": "./static/js/runtime-main.81fef7fb.js"
+  }
+]);
